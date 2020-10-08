ceb61c0a7d418cc5b468ff2783dd71c215c49ad4
jixiaoxing <1144760915@qq.com>
2020-10-08 15:56:00 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -8,7 +8,23 @@ var baseurl = 'http://ajax.frontend.itheima.net';
 //拦截所有ajax\post\get请求
 //配置参数
 $.ajaxPrefilter(function(options) {
-    alert(options.url)
+    //2.为以/my/开头的Ajax请求设置头信息
+    options.headers = {
+        Authorization: localStorage.getItem('token') || '',
+    }
+
+    //1.拼接url地址
+    // alert(options.url)
     options.url = baseurl + options.url;
-    alert(options.url)
+    // alert(options.url)
+
+    //3.拦截所有响应，判断身份认证信息
+    //无论成功或失败，都要调用这个回调函数
+    options.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+$(function() {
+    getUserInfo();
+    //退出功能
+    $("#btnlogout").on('click', function() {
+        layer.confirm('是否确定退出登录?', {
+            icon: 3,
+            title: '提示'
+        }, function(index) {
+            //do something
+            //1.删除本地存储token
+            localStorage.removeItem('token');
+            //2.跳转至登录页面
+            location.href = '/login.html';
+            //3.layui自己提供的关闭弹出层的功能
+            layer.close(index);
+        });
+    })
+});
+
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || '',
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            };
+            renderAvatar(res.data) //调用头像函数
+        },
+        //无论成功或失败，都要调用这个回调函数
+        // complete: function(res) {
+        //     console.log(res);
+        //     if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+        //         localStorage.removeItem('token');
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+};
+
+//封装头像函数
+function renderAvatar(user) {
+    //1.获取用户名
+    var name = user.nickname || user.username;
+    //2.渲染文字
+    $("#welcome").html('欢迎&nbsp;&nbsp' + name);
+    //3.渲染头像
+    var first = name[0].toUpperCase()
+    if (user.user_pic !== null) {
+        //3.1渲染图片头像
+        $(".layui-nav-img").show().prop('src', user.user_pic);
+        $(".user-avatar").hide()
+    } else {
+        //3.2渲染文字头像
+        $(".layui-nav-img").hide();
+        $(".user-avatar").show().html(first)
+    }
+}
